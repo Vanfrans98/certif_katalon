6556b2c6301bcc81469639eddd38847ec37245e2
frans <frans@VanFrans1998>
2025-03-16 09:27:55 +0700

    commit 16-03-2025

--- Test Suites/sauceDemo.ts
+++ Test Suites/sauceDemo.ts
@@ -12,20 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>057d302c-f677-4a85-b5f5-fbca87594070</testSuiteGuid>
    <testCaseLink>
-      <guid>c932b1ee-5125-455f-a055-45e7da5d6e62</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/berhasil_login</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d6c281ad-d7be-4c3b-b2fd-07214cf9a226</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/gagal_login</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>a7c5e91a-22bc-4d52-926a-668daf81a1d3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
